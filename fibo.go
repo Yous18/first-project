f53d8fb2f609cc98ef83be04809cd3ba5b594676
Yous18 <97679946+Yous18@users.noreply.github.com>
2022-01-26 18:21:41 +0200

    Update fibo.go

--- fibo.go
+++ fibo.go
@@ -9,12 +9,12 @@ func main() {
 	
 
 }
-func fiboList(UserNumber int) {
+func getNearestFibo (UserNumber int) {
 	var n1 int = 0
 	var n2 int = 1
 	var n3 int = n2
 	fmt.Printf("FiboList: %v", n1)
-	for {
+	for n := range UserNumber {
 		n3 = n2
 		n2 = n1 + n2
 		if n2 >= x {
